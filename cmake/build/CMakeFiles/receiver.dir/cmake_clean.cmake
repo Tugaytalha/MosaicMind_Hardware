7a7cdc7c032ebdf7e6f5f4c6a5e786652170af23
Ali Erdem Akyüz <alierdemakyuz61@gmail.com>
2024-05-31 09:58:18 +0300

    clean up

---
+++ cmake/build/CMakeFiles/receiver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/receiver.dir/receiver.cc.o"
+  "CMakeFiles/receiver.dir/receiver.cc.o.d"
+  "receiver"
+  "receiver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/receiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
